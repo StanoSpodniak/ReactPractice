06f74f418c2791a689787e1f38ae4f3f43f9c547
StanoSpodniak <stano28@gmail.com>
2024-06-24 19:44:49 +0200

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import ListGroup from "./components/ListGroup";
-//continue 60 min. https://www.youtube.com/watch?v=SqcY0GlETPk&t=10s
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+//import ListGroup from "./components/ListGroup";
+//Course https://codewithmosh.com/p/ultimate-react-part1
 
-let items = ['New York',  'San Francisco', 'Tokyo', 'London', 'Paris'];
+/*let items = ['New York',  'San Francisco', 'Tokyo', 'London', 'Paris'];
 
 const handleSelectItem = (item: string) => {
   console.log(item);
-}
+}*/
 
 function App() {
+  const[alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+      {/*<ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>*/}
+      {alertVisible && 
+      <Alert onClose={() => {setAlertVisibility(false)}}>
+        My alert
+      </Alert>}
+      <Button color="primary" onClick={() => {setAlertVisibility(true)}}>My Button</Button>
     </div>
   );
 }
