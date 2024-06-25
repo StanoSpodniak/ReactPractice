bbab47ef5ed18b14a963cc384ecac8a11d517086
StanoSpodniak <stano28@gmail.com>
2024-06-25 14:22:19 +0200

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
 //import ListGroup from "./components/ListGroup";
-//Course https://codewithmosh.com/p/ultimate-react-part1
+//import Alert from "./components/Alert";
+//import { BsFillCalendarFill } from 'react-icons/bs';
+import Like from "./components/Like";
+import Button from "./components/Button/Button";
+
+//Course https://members.codewithmosh.com/courses/ultimate-react-part1-1/lectures/45915382
 
 /*let items = ['New York',  'San Francisco', 'Tokyo', 'London', 'Paris'];
 
@@ -15,12 +18,14 @@ function App() {
 
   return (
     <div>
-      {/*<ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>*/}
-      {alertVisible && 
+      {/*<ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+      <BsFillCalendarFill color="red" size="40" />*/}
+      {/*{alertVisible && 
       <Alert onClose={() => {setAlertVisibility(false)}}>
         My alert
-      </Alert>}
-      <Button color="primary" onClick={() => {setAlertVisibility(true)}}>My Button</Button>
+      </Alert>}*/}
+      <Button color="danger" onClick={() => {setAlertVisibility(true)}}>My Button</Button>
+      <Like onClick={() => console.log("clicked")} />
     </div>
   );
 }
--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import styles from './ListGroup.module.css';
+
 interface Props {
     items: string[];
     heading: string;
@@ -12,7 +14,7 @@ function ListGroup({items, heading, onSelectItem}: Props) {
         <>
             <h1>{heading}</h1>
             {items.length === 0 && <p>No items found</p>}
-            <ul className="list-group">
+            <ul className={[styles.listGroup, styles.container].join(' ')}>
                 {items.map((item, index) => (
                     <li 
                         className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
